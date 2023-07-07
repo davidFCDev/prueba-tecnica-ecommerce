7016fffd50a76eebfbbaf311d8c830ba665f9542
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-07 13:21:13 +0200

    añadidos filtros

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Filters.css";
 
-export function Filters() {
+export function Filters({ onChange }) {
+  const [minPrice, setMinPrice] = useState(0);
+
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value);
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
+  const handleChangeCategory = (event) => {
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
   return (
     <section className="filters">
+    
       <div>
-        <label htmlFor="price">Price</label>
-        <input type="range" id="price" min={0} max={1000} />
+        <label htmlFor="price">Price from:</label>
+        <input
+          type="range"
+          id="price"
+          min={0}
+          max={1000}
+          onChange={handleChangeMinPrice}
+        />
+        <span>${minPrice}</span>
       </div>
 
       <div>
         <label htmlFor="category">Category</label>
-        <select id="category">
+        <select id="category" onChange={handleChangeCategory}>
           <option value="all">All</option>
           <option value="laptops">Laptops</option>
           <option value="smartphones">Smartphones</option>
